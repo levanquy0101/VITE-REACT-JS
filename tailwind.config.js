fc5c045da6414732f359a65f44638f314d44e0c4
Lê Văn Quý <138227498+levanquy0101@users.noreply.github.com>
2024-08-07 17:09:27 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/ui/**/*.{js,ts,jsx,tsx}", // Include only files in the src/ui directory
+  ],
+  corePlugins: {
+    preflight: false, // Disable default CSS
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
